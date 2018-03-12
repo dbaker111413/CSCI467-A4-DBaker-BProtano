31f67b5831be0eaa50fd076054ac08edf2d92b78
Bradley Protano <z1772315@hopper.cs.niu.edu>
2018-03-12 11:02:15 -0500

    database creation script

---
+++ a4.sql
@@ -0,0 +1,45 @@
+/*
+item(item_id, description, uom, location_id, on_hand,price, barcode)
+customer(customer_id, name, rep_lname,rep_fname, email, phone, shipping_id, billing_id,
+		username, password, comments)
+address(address_id, address_line1, address_line2, city, state, zip)
+*/
+
+drop table if exists item;
+drop table if exists customer;
+drop table if exists address;
+
+create table item
+	(item_id int NOT NULL auto_increment,
+	description varchar(100),
+	uom varchar (25),
+	location_id int,
+	on_hand int,
+	price double,
+	barcode int
+	primary key (item_it));
+
+insert into item (description, uom, location_id, on_hand, price)
+	values ('1/3 inch Lugnut', 'box (16 nuts per)', '1', '20', '5.89');
+
+create table customer
+	(customer_id int NOT_NULL auto_increment,
+	name varchar (25),
+	rep_lname varchar (25),
+	rep_fname varchar (25),
+	email varchar (25),
+	phone varchar (14),
+	shipping_id int,
+	billing_id int,
+	username. varchar (10),
+	password varchar (10)
+	comments varchar (1000),
+	primary key (customer_id));
+
+create table address
+	(address_id int NOT_NULL auto_increment,
+	address_line1 varchar (25),
+	address_line2 varchar (25),
+	city varchar (25),
+	zip varchar (6),
+	primary key (address_id));
