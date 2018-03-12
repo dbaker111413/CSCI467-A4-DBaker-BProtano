961d3f7cc447bbe85f52b9d08e26b84ea905555a
Daniel Baker <z1790145@turing.cs.niu.edu>
2018-03-12 11:19:25 -0500

    fixed up typos

--- a4.sql
+++ a4.sql
@@ -16,28 +16,25 @@ create table item
 	location varchar (25),
 	on_hand int,
 	price double,
-	primary key (item_it));
-
-insert into item (description, uom, location_id, on_hand, price)
-	values ('1/3 inch Lugnut', 'box (16 nuts per)', 'Dock 1', '20', '5.89');
+	primary key (item_id));
 
 create table customer
-	(customer_id int NOT_NULL auto_increment,
+	(customer_id int NOT NULL auto_increment,
 	name varchar (25),
 	rep_lname varchar (25),
 	rep_fname varchar (25),
 	email varchar (25),
-	phone varchar (10)
+	phone varchar (10),
 	phone_ext varchar (4),
 	shipping_id int,
 	billing_id int,
-	username. varchar (10),
-	password varchar (10)
+	username varchar (10),
+	password varchar (10),
 	comments varchar (280),
 	primary key (customer_id));
 
 create table address
-	(address_id int NOT_NULL auto_increment,
+	(address_id int NOT NULL auto_increment,
 	address_line1 varchar (25),
 	address_line2 varchar (25),
 	city varchar (25),
