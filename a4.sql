c50961b5eba48901145fc0e9107bd724bfcfe535
Bradley Protano <z1772315@hopper.cs.niu.edu>
2018-03-12 11:10:58 -0500

    database creation script fin

--- a4.sql
+++ a4.sql
@@ -13,14 +13,13 @@ create table item
 	(item_id int NOT NULL auto_increment,
 	description varchar(100),
 	uom varchar (25),
-	location_id int,
+	location varchar (25),
 	on_hand int,
 	price double,
-	barcode int
 	primary key (item_it));
 
 insert into item (description, uom, location_id, on_hand, price)
-	values ('1/3 inch Lugnut', 'box (16 nuts per)', '1', '20', '5.89');
+	values ('1/3 inch Lugnut', 'box (16 nuts per)', 'Dock 1', '20', '5.89');
 
 create table customer
 	(customer_id int NOT_NULL auto_increment,
@@ -28,12 +27,13 @@ create table customer
 	rep_lname varchar (25),
 	rep_fname varchar (25),
 	email varchar (25),
-	phone varchar (14),
+	phone varchar (10)
+	phone_ext varchar (4),
 	shipping_id int,
 	billing_id int,
 	username. varchar (10),
 	password varchar (10)
-	comments varchar (1000),
+	comments varchar (280),
 	primary key (customer_id));
 
 create table address
