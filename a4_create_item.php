883978cf4afb55565e4fbc2c41f55d8e54e4b0d4
Daniel Baker <danbaker951@gmail.com>
2018-03-14 22:21:28 -0500

    testing stuff

--- a4_create_item.php
+++ a4_create_item.php
@@ -50,23 +50,28 @@
 /**************************************************/
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['which']) == 'create') {
-      $itemDesc = $_POST['itemDesc'];
-      $uom = $_POST['uom'];
-      $warehouseLoc = $_POST['warehouseLoc'];
-      $qty = $_POST['qty'];
-      $price = $_POST['price'];
+      // confirm that the user actually wants to insert this item
+      echo "<script type='text/javascript'>confirm('Are you sure you want to create this item?');</script>";
+      if(confirm("Are you sure you want to create this item?")){
+        $itemDesc = $_POST['itemDesc'];
+        $uom = $_POST['uom'];
+        $warehouseLoc = $_POST['warehouseLoc'];
+        $qty = $_POST['qty'];
+        $price = $_POST['price'];
       
-      $insertSQL = 'insert into item ( description, uom, location, on_hand, price)
+        $insertSQL = 'insert into item ( description, uom, location, on_hand, price)
 	     		               values (?, ?, ?, ?, ?)';
  
-      try {
-        $stmt = $conn->prepare($insertSQL);
-	$ok = $stmt->execute(array($itemDesc, $uom, $warehouseLoc, $qty, $price));
-        echo ' Item added successfully!';
-      }
-      catch (PDOException $e) {
-        echo 'Error, customer could not be added.';
-        echo 'Error: '.$e->getMessage();
+        try {
+          $stmt = $conn->prepare($insertSQL);
+	  $ok = $stmt->execute(array($itemDesc, $uom, $warehouseLoc, $qty, $price));
+	  $message = "Item added successfully!";
+          echo "<script type='text/javascript'>alert('$message');</script>";
+        }
+        catch (PDOException $e) {
+          echo 'Error, customer could not be added.';
+          echo 'Error: '.$e->getMessage();
+        }
       }
     }
   }
