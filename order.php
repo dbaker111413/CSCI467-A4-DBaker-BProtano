6dca0652fc839b71e99bf0ed97d5cad40da37004
Bradley Protano <z1772315@hopper.cs.niu.edu>
2018-03-24 15:45:49 -0500

    th worst code man has ever seen, broken customr order

--- order.php
+++ order.php
@@ -4,27 +4,14 @@
 require_once("conn.php");
 require_once("globalFunctions.php");
 
-class customer {
-  public $customerNumber = 0;
-  public $customerName = "";
-  public $billAddress1 = "";
-  public $billAddress2 = "";
-  public $billCity = "";
-  public $billState = "";
-  public $billZip = "";
-  public $shipAddres1 = "";
-  public $shipAddress2 = "";
-  public $shipCity = "";
-  public $shipState = "";
-  public $shipZip = "";
-  public $fname = "";
-  public $lname = "";
-  public $areaCode = "";
-  public $midDigits = "";
-  public $endDigits = "";
-  public $ext = "";
-  public $email = "";
-  public $comment = "";
+class order {
+  public $orderNum = 0;
+  public $date = "";
+  public $status= "";
+  public $expectedDate= "";
+  public $lines = 0;
+  public $customerNum = 0;
+  public $shipmentNum = 0;
   public $conn;
 
   /*
@@ -35,24 +22,19 @@ class customer {
      $this->conn = $conn;
   }
 
+
   /*
   * inserts new customer into database.
   * returns true if successful, false otherwisex
   **/
   public function addToDatabase(){
      // data is validated as part of the html definition
-     $insertSQL = 'insert into customer ( name, address_line1, address_line2, city, state, zip,
-					  ship_address_line1, ship_address_line2, ship_city,
-                                          ship_state, ship_zip, fname, lname, phone_area,
-                                          phone_middle, phone_end, phone_ext, email, comments )
-	     		                  values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+     $insertSQL = 'insert into order_header ( order_date, order_status, order_expected_date, order_lines, customer_id,
+	     		                  values (?, ?, ?, ?, ?)';
   
      try {
        $stmt = $this->conn->prepare($insertSQL);
-       $ok = $stmt->execute(array($this->customerName, $this->billAddress1, $this->billAddress2, $this->billCity, $this->billState, $this->billZip,
-	                           $this->shipAddress1, $this->shipAddress2, $this->shipCity, $this->shipState, $this->shipZip, $this->fname,
-	                           $this->lname, $this->areaCode, $this->midDigits, $this->endDigits, $this->ext, $this->email, 
-				   $this->comment));
+       $ok = $stmt->execute(array($this->date, $this->status, $this->expectedDate, $this->lines, $this->customer_id));
 
 
        $message = "Customer Order created successfully!";
