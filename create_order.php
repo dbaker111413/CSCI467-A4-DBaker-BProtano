30e14b96a218be26fe6dc142cf360f96e880ac67
Daniel Baker <danbaker951@gmail.com>
2018-03-28 00:31:59 -0500

    fixed add button

--- create_order.php
+++ create_order.php
@@ -20,6 +20,7 @@
     global $lineCounter;
     $outputLine = "";
 
+    
     while(isset($_POST["itemnum".$lineCounter])){
       $outputLine .= generateSingleDetailLine();
       $lineCounter++;
@@ -31,12 +32,12 @@
   function generateSingleDetailLine(){
     global $lineCounter, $itemDropDownMenu;
     return '<tr>
-	    <td><select id="itemnum"'.$lineCounter.' name="itemnum"'.$lineCounter.'><option>-- Select Item --</option>'.$itemDropDownMenu.'</select></td>
-	    <td><label id="price"'.$lineCounter.' name="price"'.$lineCounter.'>$0.00</label></td>
-	    <td><label id="qty"'.$lineCounter.' name="qty"'.$lineCounter.'>0</label></td>
-	    <td><label id="uom"'.$lineCounter.' name="uom"'.$lineCounter.'>--</label></td>
-	    <td><label id="total"'.$lineCounter.' name="total"'.$lineCounter.'>$0.00</label></td>
-	    <td><button type="button" id="delete"'.$lineCounter.' name="delete"'.$lineCounter.' onclick="delete_line()" value="0">DELETE</button></td>
+	    <td><select id="itemnum'.$lineCounter.'" name="itemnum'.$lineCounter.'"><option>-- Select Item --</option>'.$itemDropDownMenu.'</select></td>
+	    <td><label id="price'.$lineCounter.'" name="price'.$lineCounter.'">$0.00</label></td>
+	    <td><label id="qty'.$lineCounter.'" name="qty'.$lineCounter.'">0</label></td>
+	    <td><label id="uom'.$lineCounter.'" name="uom'.$lineCounter.'">--</label></td>
+	    <td><label id="total'.$lineCounter.'" name="total'.$lineCounter.'">$0.00</label></td>
+	    <td><button type="button" id="delete'.$lineCounter.'" name="delete'.$lineCounter.'" onclick="delete_line()" value="0">DELETE</button></td>
 	  </tr>';
   }
 
@@ -49,11 +50,6 @@
       showAlert("generating new line item");
       $htmlDetailLines .= generateSingleDetailLine();
       $lineCounter++;
-    }
-
-
-    if(isset($_POST['itemnum0'])){
-      showAlert($_POST['itemnum0']);
     }
   }
 
