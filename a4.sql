7cf1d3a414c20d703f0ce31c8ece0b62c3304490
Bradley Protano <z1772315@hopper.cs.niu.edu>
2018-04-02 10:19:48 -0500

    new predefined DB with dummy values

--- a4.sql
+++ a4.sql
@@ -25,13 +25,13 @@ create table vendor
 	primary key (vendor_id));
 
 insert into vendor (name)
-        values ('Mobius One');
+        values ('Cessna Aircraft Company');
 insert into vendor (name)
-        values ('Mobius Two');
+        values ('Honeywell');
 insert into vendor (name)
-        values ('Mobius Three');
+        values ('General Electric');
 insert into vendor (name)
-        values ('Not Mobius');
+        values ('ACSS');
 
 create table item
 	(item_id int NOT NULL auto_increment,
@@ -45,11 +45,12 @@ create table item
         foreign key (vendor_id) references vendor(vendor_id));
 
 insert into item (description, uom, location, on_hand, price, vendor_id)
-        values ('Metal Gear', 'box (4 pieces)', "Groznj grad", 12, 59.99, 1),
-               ('Soliton radar', 'box (1 piece)', "Shadow Moses", 4, 39.99, 1),
-               ('NVG solid eye', 'box (6 pieces)', "Camp Oemga", 16, 159.99, 1),
-               ('C. Box (The Orange)', 'box (1 per duh)', "Big Shell", 42, 10.99, 1),
-               ('Active Decoy', 'box (4 pieces)', "Mother Base", 30, 255.99, 1);
+        values ('A32 Rudder', 'Crate (8 pieces)', "Aisle 3A, Bin 2", 22, 759.99, 1),
+               ('RNAV/GNSS radar', 'Box (1 piece)', "Aisle 1C, Bin 5", 4, 1239.99, 4),
+               ('Stroboscopic Navigation Light', 'Box (12 pieces)', "Aisle 4 Row 4", 16, 159.99, 2),
+               ('1/2 inch Ball Bearings', 'Box (80 pieces)', "Aisle 1C Bin 4", 424, 30.99, 3),
+               ('B12 Hydralic Fluid', '4 Gallons', "Aisle 5 Shelf 1", 60, 290.00, 3),
+               ('C17 Yoke', 'Box (1 piece)', "Aisle 2A Row 4", 13, 890.00, 1);
 
 create table customer
 	(customer_id int NOT NULL auto_increment,
@@ -76,12 +77,17 @@ create table customer
 	comments varchar (280),
 	primary key (customer_id));
 
-insert into customer (name, ship_address_line1, ship_address_line2, ship_city, ship_state, ship_zip)
-        values ('Outer Heaven', '014 Intrude ave.', 'Tower Building', 'Zanzibar', 'IL', '60115');
+insert into customer (name, ship_address_line1, ship_address_line2, ship_city, ship_state, ship_zip, address_line1, address_line2, city, state, zip, lname, fname, email,
+                        phone_area, phone_middle, phone_end, comments)
+        values ('Belkan', '2014 Normal Rd.', 'Building 2', 'Dekalb', 'IL', '60115', '110 E. John St.', '103', 'Champaign', 'IL', '61820', 'Protano', 'Bradley', 'not@real.email',
+	          555,555,5555, 'This is placeholder text.'),
+               ('Mobius Cor', '110 E. John St.', '103', 'Champaign', 'IL', '61820', '2014 Normal Rd.', 'Building 2', 'Dekalb', 'IL', '60115', 'Baker', 'Daniel', 'so@real.email',
+          	  555,555,5555, 'This text is significant'),
+               ('ArmsTech', '1800 W. Foster Ave.', 'Floor 3', 'Chicago', 'IL', '60625', '110 E. John St.', '103', 'Champaign', 'IL', '61820', 'Protano', 'Bradley','not@aip.email',
+	          555,555,5555, 'This is text that could include preferences that do not fit a variable.'),
+               ('Feisar', '312 Auburn Rd.', 'Rm 130', 'Elgin', 'IL', '60170',  '2014 Normal Rd.', 'Building 2', 'Dekalb', 'IL', '60115', 'Baker', 'Daniel', 'so@real.email',
+          	  555,555,5555, 'This text is significant and meaninfdul');
 
-insert into customer (name)
-        values ('Stonehenge'),
-               ('Yellow 13');
 
 create table order_header
        (order_id int NOT NULL auto_increment,
@@ -95,29 +101,14 @@ create table order_header
        foreign key (customer_id) references customer(customer_id));
 
 insert into order_header (order_date, order_status, order_expected_date, order_lines, customer_id)
-        values (current_timestamp, 'Released for pick', '2018-01-23', 5, 1),
-               (current_timestamp, 'Released for pick', '2018-02-23', 2, 2),
-               (current_timestamp, 'Released for pick', '2018-03-23', 6, 3),
-               (current_timestamp, 'Released for pick', '2018-04-23', 3, 1),
-               (current_timestamp, 'Released for pick', '2018-05-23', 4, 2),
-               (current_timestamp, 'Released for pick', '2018-06-23', 4, 1),
-               (current_timestamp, 'Released for pick', '2018-07-23', 4, 3),
-               (current_timestamp, 'Released for pick', '2018-08-23', 4, 2),
-               (current_timestamp, 'Released for pick', '2018-09-23', 4, 1),
-               (current_timestamp, 'Released for pick', '2018-11-23', 4, 3),
-               (current_timestamp, 'Released for pick', '2018-12-23', 4, 3),
-               (current_timestamp, 'Released for pick', '2018-05-24', 4, 2),
-               (current_timestamp, 'Released for pick', '2018-05-25', 4, 2),
-               (current_timestamp, 'Released for pick', '2018-05-26', 4, 1),
-               (current_timestamp, 'Released for pick', '2018-05-27', 4, 1),
-               (current_timestamp, 'Released for pick', '2018-05-28', 4, 2),
-               (current_timestamp, 'Released for pick', '2018-05-12', 4, 1),
-               (current_timestamp, 'Released for pick', '2018-05-13', 4, 3),
-               (current_timestamp, 'Released for pick', '2018-05-14', 4, 2),
-               (current_timestamp, 'Released for pick', '2018-05-15', 4, 2),
-               (current_timestamp, 'Released for pick', '2018-05-17', 4, 2),
-               (current_timestamp, 'Released for pick', '2018-05-16', 4, 1),
-               (current_timestamp, 'Released for pick', '2018-06-23', 2, 1);
+        values (current_timestamp, 'Released for pick', '2018-04-05', 6, 1),
+               (current_timestamp, 'Released for pick', '2018-04-16', 7, 2),
+               (current_timestamp, 'Customer Approved', '2018-04-24', 3, 2),
+               (current_timestamp, 'Customer Approved', '2018-04-27', 2, 3),
+               (current_timestamp, 'Released for pick', '2018-04-29', 5, 3),
+               (current_timestamp, 'Released for pick', '2018-05-02', 12, 4),
+               (current_timestamp, 'Customer Approved', '2018-05-03', 1, 1),
+               (current_timestamp, 'Created', '2018-08-17', 3, 4);
 
 create table detail
        (line_id int NOT NULL auto_increment,
@@ -128,11 +119,42 @@ create table detail
        foreign key (order_id) references order_header(order_id));
 
 insert into detail (order_id, item_id, line_qty)
-        values (1, 1, 4),
-               (1, 2, 1),
+        values (1, 2, 4),
+               (1, 1, 1),
                (1, 3, 2),
                (1, 4, 15),
+               (1, 6, 6),
                (1, 5, 6),
-               (23, 3, 1),
-               (23, 4, 4),
-               (23, 5, 14);
+               (3, 2, 14),
+               (3, 1, 12),
+               (3, 3, 8),
+               (2, 3, 15),
+               (2, 6, 6),
+               (2, 4, 18),
+               (2, 2, 4),
+               (2, 1, 1),
+               (2, 3, 2),
+               (2, 5, 15),
+               (4, 6, 4),
+               (4, 5, 10),
+               (5, 1, 40),
+               (5, 2, 13),
+               (5, 3, 3),
+               (5, 4, 22),
+               (5, 5, 6),
+               (6, 5, 8),
+	       (6, 2, 9),
+               (6, 1, 11),
+               (6, 3, 24),
+               (6, 4, 35),
+               (6, 6, 7),
+               (6, 5, 18),
+	       (6, 2, 25),
+               (6, 1, 1),
+               (6, 3, 4),
+               (6, 4, 8),
+               (6, 6, 12),
+               (7, 5, 6),
+               (8, 3, 1),
+               (8, 4, 120),
+               (8, 5, 14);
